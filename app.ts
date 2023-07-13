feebbd504e5c2219b14d09f3b5bc8b41fb4841f5
pratikstemkar <pratikstemkar@gmail.com>
2023-07-13 18:23:32 +0530

    socket server basic

--- app.ts
+++ app.ts
@@ -1,11 +1,33 @@
 const express = require("express");
+var cors = require("cors");
+const http = require("http");
 const app = express();
+const { Server } = require("socket.io");
 const port = 3000;
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:5173",
+		methods: ["GET", "POST"],
+	},
 });
 
-app.listen(port, () => {
-	console.log(`App listening to port ${port}.`);
+io.on("connection", (socket) => {
+	console.log(`User Connected: ${socket.id}`);
+
+	socket.on("join_room", (data) => {
+		socket.join(data);
+	});
+
+	socket.on("send_message", (data) => {
+		socket.to(data.room).emit("receive_message", data);
+	});
+});
+
+server.listen(port, () => {
+	console.log(`listening to ${port} ...`);
 });
